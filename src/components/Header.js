281e9208d996cd6e2e9720a8aa4c048aa2508e4d
Debam Roy <debamroy7@gmail.com>
2024-02-29 01:36:30 +0530

    nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,46 @@ import Socials from './Socials';
 import Logo from '../img/header/logo.svg';
 import MobileNav from './MobileNav';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
-    <div className="flex "></div>
+    <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+
+      {/* -------------logo------------------ */}
+      <Link to={'/'}>
+        <img src={Logo} alt='' className='max-w-[200px]' />
+      </Link>
+
+      {/* ------------------navbar------------ */}
+      <nav className='hidden xl:flex gap-x-12 font-semibold'>
+        <Link
+          to={'/'} className="text-[#696c6d] hover:text-primary transition">
+          Home
+        </Link>
+        <Link
+          to={'/about'} className="text-[#696c6d] hover:text-primary transition">
+          About
+        </Link>
+        <Link
+          to={'/portfolio'} className="text-[#696c6d] hover:text-primary transition">
+          Portfolio
+        </Link>
+        <Link
+          to={'/contact'} className="text-[#696c6d] hover:text-primary transition">
+          Contact
+        </Link>
+      </nav>
+
+    </div>
+
+    {/* ----------------socials---------------- */}
+    <Socials />
+
+    {/* -----------------mobile nav------------ */}
+    <MobileNav />
+
+
   </header>;
 };
 
