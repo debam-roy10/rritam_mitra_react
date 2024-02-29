79c4cd91bfe22415abdace21de6183bf336bd0ef
Debam Roy <debamroy7@gmail.com>
2024-02-29 16:32:30 +0530

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+import AboutBg from '../img/about/aboutbg.png';
+
+import { Link } from 'react-router-dom';
+
 const About = () => {
-  return <section className='section bg-yellow-200'>About</section>;
+  return <section className='section'>
+    <div className="container mx-auto h-full relative">
+      <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+
+        <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+          <img src={AboutBg} alt='' className='w-[689px]'/>
+        </div>
+
+        <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1'>
+            About me
+          </h1>
+          <p className='mb-12 max-w-sm'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            <b> Itaque, suscipit aliquid asperiores </b>cumque enim sint dignissimos excepturi exercitationem.
+            <br />
+            <br />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+
+          <Link to={'/portfolio'} className='btn'>View my work</Link>
+
+        </div>
+
+      </div>
+    </div>
+  </section>;
 };
 
 export default About;
