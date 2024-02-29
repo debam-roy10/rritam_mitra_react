4d076bb074f4fca35e6fa1af03dce21829dbc90c
Debam Roy <debamroy7@gmail.com>
2024-02-29 19:07:54 +0530

    final

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -8,13 +8,13 @@ const Socials = () => {
   return (
     <div className='hidden xl:flex ml-24'>
       <ul className='flex gap-x-4'>
-        <li>
+        <li className='text-[#5a5d5e] hover:text-primary transition'>
           <a href="https://www.facebook.com/rritam.mitra?mibextid=ZbWKwL" target='_blank' rel="noreferrer"><FiFacebook /></a>
         </li>
-        <li>
+        <li className='text-[#5a5d5e] hover:text-primary transition'>
           <a href="https://twitter.com/?lang=en" target='_blank' rel="noreferrer"><FaXTwitter /></a>
         </li>
-        <li>
+        <li className='text-[#5a5d5e] hover:text-primary transition'>
           <a href="https://www.instagram.com/rritamsphotographyofficial?igsh=OW94bmJmdGZzZWN0" target='_blank' rel="noreferrer"><BsInstagram /></a>
         </li>
       </ul>
